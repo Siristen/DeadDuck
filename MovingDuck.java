673d6c2a9000a325779045e7c78ad27d33837119
Lord of Adminster <ladminster@localhost.localdomain>
2017-11-30 19:31:32 +0100

    Edit

---
+++ MovingDuck.java
@@ -0,0 +1,43 @@
+
+import java.io.*;
+import javax.swing.JApplet;
+import java.awt.image.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.imageio.*;
+import javax.swing.*;
+import javax.imageio.stream.*;
+import java.awt.Graphics;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ladminster
+ */
+public class MovingDuck {
+    
+    public MovingDuck(){
+    
+    }
+    public void drawDuck()throws IOException{
+//This will draw the duck
+BufferedImage duckie = ImageIO.read(new File("/home/ladminster/git/DeadDuck/Duck.jpg"));
+
+
+}
+    
+   public void MoveDuck(){
+
+//This will redraw the duck
+}
+    
+}
